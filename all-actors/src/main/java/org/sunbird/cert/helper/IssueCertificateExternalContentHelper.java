ec55f40ff6c76a2e6de2e77a0cb738c6c6c7eec7
sahilchaudhary <sahil.chaudhary@tarento.com>
2025-12-23 19:51:30 +0530

    Adding the commit for extensive course for external Courses

--- all-actors/src/main/java/org/sunbird/cert/helper/IssueCertificateExternalContentHelper.java
+++ all-actors/src/main/java/org/sunbird/cert/helper/IssueCertificateExternalContentHelper.java
@@ -165,7 +165,7 @@ public class IssueCertificateExternalContentHelper {
                 eData.put("courseNameExtended", courseNameExtended);
             } else {
                 eData.put("courseName", courseName);
-                eData.put("courseNameExtended", "&nbsp;");
+                eData.put("courseNameExtended", "\u200B");
             }
             logger.info("The edata is updated : " + eData.get("courseName") + " : testing" +eData.get("courseNameExtended"));
             eData.put("basePath", PropertiesCache.getInstance().getProperty("cert_domain_url") + "/certs");
